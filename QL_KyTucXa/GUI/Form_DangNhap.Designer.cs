2976bd40de5433fde2facd3e8b61123ef080c913
Minh Thuong <pijnsu@gmail.com>
2024-12-14 11:37:08 +0700

    Push form

--- QL_KyTucXa/GUI/Form_DangNhap.Designer.cs
+++ QL_KyTucXa/GUI/Form_DangNhap.Designer.cs
@@ -28,12 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form_DangNhap";
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Cambria", 13.8F, FontStyle.Bold);
+            label1.Location = new Point(206, 140);
+            label1.Name = "label1";
+            label1.Size = new Size(131, 27);
+            label1.TabIndex = 0;
+            label1.Text = "TÀI KHOẢN";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Cambria", 13.8F, FontStyle.Bold);
+            label2.Location = new Point(206, 233);
+            label2.Name = "label2";
+            label2.Size = new Size(127, 27);
+            label2.TabIndex = 0;
+            label2.Text = "MẬT KHẨU";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F);
+            textBox1.Location = new Point(343, 140);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(201, 34);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F);
+            textBox2.Location = new Point(343, 226);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(201, 34);
+            textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Cambria", 13.8F, FontStyle.Bold);
+            button1.Location = new Point(395, 307);
+            button1.Name = "button1";
+            button1.Size = new Size(194, 59);
+            button1.TabIndex = 2;
+            button1.Text = "Đăng nhập";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Cambria", 13.8F, FontStyle.Bold);
+            button2.Location = new Point(171, 307);
+            button2.Name = "button2";
+            button2.Size = new Size(194, 59);
+            button2.TabIndex = 2;
+            button2.Text = "Đăng xuất";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Cambria", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(286, 48);
+            label3.Name = "label3";
+            label3.Size = new Size(181, 36);
+            label3.TabIndex = 3;
+            label3.Text = "ĐĂNG NHẬP";
+            // 
+            // Form_DangNhap
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form_DangNhap";
+            Text = "Form_DangNhap";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+        private Label label3;
     }
 }
--- QL_KyTucXa/GUI/Form_Menu.Designer.cs
+++ QL_KyTucXa/GUI/Form_Menu.Designer.cs
@@ -28,18 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            menuStrip1 = new MenuStrip();
+            đĂNGXUÂTToolStripMenuItem = new ToolStripMenuItem();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Font = new Font("Cambria", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox1.Location = new Point(53, 88);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(313, 517);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "HỆ THỐNG";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button6);
+            groupBox2.Controls.Add(button5);
+            groupBox2.Controls.Add(button4);
+            groupBox2.Font = new Font("Cambria", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox2.Location = new Point(453, 88);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(313, 517);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "QUẢN LÍ";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(47, 47);
+            button1.Name = "button1";
+            button1.Size = new Size(222, 130);
+            button1.TabIndex = 2;
+            button1.Text = "ĐĂNG KÝ PHÒNG";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(47, 194);
+            button2.Name = "button2";
+            button2.Size = new Size(222, 130);
+            button2.TabIndex = 2;
+            button2.Text = "THÔNG BÁO";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(47, 345);
+            button3.Name = "button3";
+            button3.Size = new Size(222, 130);
+            button3.TabIndex = 2;
+            button3.Text = "THỐNG KÊ";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Cambria", 13.8F, FontStyle.Bold);
+            button4.Location = new Point(44, 47);
+            button4.Name = "button4";
+            button4.Size = new Size(222, 130);
+            button4.TabIndex = 3;
+            button4.Text = "QUẢN LÍ NHÂN VIÊN";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(45, 196);
+            button5.Name = "button5";
+            button5.Size = new Size(222, 130);
+            button5.TabIndex = 4;
+            button5.Text = "QUẢN LÍ PHÒNG";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(45, 345);
+            button6.Name = "button6";
+            button6.Size = new Size(222, 130);
+            button6.TabIndex = 5;
+            button6.Text = "QUẢN LÍ SINH VIÊN";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { đĂNGXUÂTToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(833, 35);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // đĂNGXUÂTToolStripMenuItem
+            // 
+            đĂNGXUÂTToolStripMenuItem.Font = new Font("Cambria", 13.8F, FontStyle.Bold);
+            đĂNGXUÂTToolStripMenuItem.Name = "đĂNGXUÂTToolStripMenuItem";
+            đĂNGXUÂTToolStripMenuItem.Size = new Size(149, 31);
+            đĂNGXUÂTToolStripMenuItem.Text = "ĐĂNG XUÂT";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Cambria", 13.8F, FontStyle.Bold);
+            label1.Location = new Point(53, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(204, 27);
+            label1.TabIndex = 3;
+            label1.Text = "MENU ĐIỀU HÀNH";
             // 
             // Form_Menu
             // 
             AutoScaleDimensions = new SizeF(9F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1188, 692);
+            ClientSize = new Size(833, 692);
+            Controls.Add(label1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Name = "Form_Menu";
             Text = "Form_Menu";
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Button button3;
+        private Button button1;
+        private Button button2;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem đĂNGXUÂTToolStripMenuItem;
+        private Label label1;
     }
 }
